9b4a6b1a8d6429329d91234d3c114e238d206965
Oyinda-Precious <ayoajiboyeoyin@gmail.com>
2026-03-19 00:13:15 +0100

    updated config file

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,23 +1,8 @@
-// import mongoose from "mongoose";
-
-// const connectDB = async () => {
-//   try {
-//     mongoose.connection.on("connected", () =>
-//       console.log("Database connected"),
-//     );
-
-//     await mongoose.connect(`${process.env.MONGODB_URI}/hotel2025`);
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// };
-
-// export default connectDB;
 import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/hotel2025`);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("✅ MongoDB Connected Successfully");
   } catch (error) {
     console.error("❌ MongoDB Connection Failed:");
@@ -27,3 +12,18 @@ const connectDB = async () => {
 };
 
 export default connectDB;
+
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/hotel2025`);
+//     console.log("✅ MongoDB Connected Successfully");
+//   } catch (error) {
+//     console.error("❌ MongoDB Connection Failed:");
+//     console.error(error);
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDB;
